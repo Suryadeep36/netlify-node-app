8c9feec01833bfc08811c9a74e650ea578ed087d
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-01-30 08:29:56 +0530

    update index.html

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 
 const api = express();
 
 const router = express.Router();
 router.get("/hello", (req, res) => {
-    res.sendFile(__dirname + "../../index.html")
+    res.sendFile(path.resolve("index.html"));
 })
 
 api.use("/api/",router);
