e768016eac9e53ddc4df707d1261de7ea4331bd1
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-01-30 08:44:30 +0530

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -8,7 +8,10 @@ const router = express.Router();
 router.get("/hello", (req, res) => {
     res.send("Hello World")
 })
-
+api.use(expess.static(path.join(__dirname, 'public')));
+api.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 api.use("/api/",router);
 
 export const handler = serverless(api);
